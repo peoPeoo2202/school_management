e6c1e280821776d1c49bd049a91d7a585c685d28
phammyduyen04 <phammyduyen04@gmail.com>
2025-12-25 01:18:15 +0700

    [teacher] - vTeachingSchedule

--- view/teacher/vTeachingSchedule.php
+++ view/teacher/vTeachingSchedule.php
@@ -132,8 +132,7 @@ if (!isset($data)) {
                     </div>
 
                 </div>
-                <form method="GET" action="../../controller/cTeachingSchedule.php">
-                    <input type="hidden" name="action" value="viewSchedule">
+                <form method="GET" action="">
                     <div class="filter-section">
                         <div class="filter-group">
                             <label>Học kỳ</label>
